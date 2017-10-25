07f3f0c3dbe315749f24cc8c6246651991ce7997
Ned Schwartz <ned@theinterned.net>
2017-10-24 23:06:30 -0400

    making room for the header

--- src/pages/privacy.js
+++ src/pages/privacy.js
@@ -7,7 +7,7 @@ const Privacy = () => (
   <Box bg={colors.primary}>
     <Box
       width={[1, 1, 1 / 2]}
-      m={['3.5rem 0 0 0', '3.5rem 0 0 0', '3.5rem auto 0 auto']}
+      m={['6rem 0 0 0', '6rem 0 0 0', '6rem auto 0 auto']}
       px={[3, 3, 0]}
       color={colors.secondary}
     >
